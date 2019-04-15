6fc260dd6634a9e3050a80c44f763ff400bcd0af
Parker Escalette <Parkerescalette@gmail.com>
2019-04-14 22:09:33 -0700

    finished token authenticating

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ https://stackoverflow.com/questions/23836000/can-i-change-the-title-bar-in-tkint
 https://stackoverflow.com/questions/14900510/changing-the-application-and-taskbar-icon-python-tkinter
 https://www.youtube.com/watch?v=Wb1YFgHqUZ8
 https://stackoverflow.com/questions/17466561/best-way-to-structure-a-tkinter-application
+https://stackoverflow.com/questions/4906977/how-to-access-environment-variable-values
+https://stackoverflow.com/questions/48883731/refresh-token-spotipy
--- classes.py
+++ classes.py
@@ -1,4 +1,93 @@
+import os
 import tkinter as tk
+import spotipy
+import spotipy.util as util
+import json
+
+SPOTIPY_CLIENT_ID = os.environ['SPOTIPY_CLIENT_ID']
+SPOTIPY_CLIENT_SECRET = os.environ['SPOTIPY_CLIENT_SECRET']
+SPOTIPY_REDIRECT_URI = 'http://localhost/'
+
+
+def getTokenCache():
+    cd = os.getcwd()
+    for root, dirs, files in os.walk(cd):
+        for file in files:
+            if '.cache-' in file:
+                return True
+                # with open(file, 'r') as cacheFile:
+                #     return json.load(cacheFile)
+    return -1
+
+
+class PlayerController():
+    def __init__(self, *args, **kwargs):
+        self.token = ''
+        self.spController = None
+
+    def getToken(self, username):
+        if not self.token:
+            scope = ('user-library-read playlist-read-private playlist-read-collaborative user-read-playback-state user-read-private user-modify-playback-state user-read-currently-playing streaming')
+            self.token = util.prompt_for_user_token(username, scope, client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET, redirect_uri=SPOTIPY_REDIRECT_URI)
+            self.spController = spotipy.Spotify(auth=self.token)
+        return self.token
+
+    def refreshToken(self):
+        cache = getTokenCache()
+        if cache != -1:
+            self.spController = spotipy.Spotify(auth=self.token)
+        pass
+
+    def getPlayback(self):
+        pass
+
+    def setPlayback(self):
+        pass
+
+    def getVolume(self):
+        pass
+
+    def setVolume(self):
+        pass
+
+    def getPlaylist(self):
+        pass
+
+    def addToPlaylist(self):
+        pass
+
+    def getPlaylists(self):
+        pass
+
+    def playSong(self):
+        pass
+
+    def playPlaylist(self):
+        pass
+
+    def playRadio(self):
+        pass
+
+    def setPlayerOptions(self):
+        pass
+
+    def nextSong(self):
+        pass
+
+    def prevSong(self):
+        pass
+
+    def getSongInfo(self):
+        pass
+
+
+class FrameController():
+    def __init__(self, *args, **kwargs):
+        self.frame = args[0]
+
+    def switchMainFrame(self, old, new):
+        pass
+
 
 
 class TitleBar(tk.Frame):
@@ -24,12 +113,9 @@ class Login(tk.Frame):
         username = tk.Frame(self.loginContainer)
         username.pack(padx=10, pady=10)
         tk.Label(username, text='Username').pack(side='left')
-        self.usernameInput = tk.Entry(username, exportselection=0).pack(side='right')
-
-        pw = tk.Frame(self.loginContainer)
-        pw.pack(padx=10, pady=10)
-        tk.Label(pw, text='Password').pack(side='left')
-        self.passwordInput = tk.Entry(pw, exportselection=0, show='*').pack(side='right')
+        self.usernameInput = tk.Entry(username, exportselection=0)
+        self.usernameInput.pack(side='right')
+        self.usernameInput.focus_set()
 
         tk.Button(self.loginContainer, text='Login', command=self.clickLogin).pack()
 
@@ -37,15 +123,17 @@ class Login(tk.Frame):
 
     def clickLogin(self, *args):
         print('clicked login')
-        # un = self.unEntry.get()
-        # pw = self.pwEntry.get()
+        username = self.usernameInput.get()
+        self.master.controller.getToken(username)
 
 
-class Player(tk.Frame):
+class App(tk.Frame):
     def __init__(self, master, *args, **kwargs):
         tk.Frame.__init__(self, master, *args, **kwargs)
         self.pack()
         self.master.title("mini player")
+        self.pController = PlayerController()
+        self.pController.refreshToken()
 
         # no resizing
         self.master.resizable(False, False)
@@ -67,8 +155,8 @@ class Player(tk.Frame):
 if __name__ == '__main__':
     root = tk.Tk()
     # root.overrideredirect(True)
-    player = Player(root)
-    player.mainloop()
+    app = App(root)
+    app.mainloop()
 
 
 
